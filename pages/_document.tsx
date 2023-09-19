2b82daaa3e89e920c282d78c02fa1d9c8452862a
tranmani <minhhuy8137@gmail.com>
2023-09-19 13:41:48 +0200

    rm logging

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
-            <script dangerouslySetInnerHTML={{ __html: logging }} />
+            {/* <script dangerouslySetInnerHTML={{ __html: logging }} /> */}
             <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
             {initialProps.styles}
             {sheet.getStyleElement()}
