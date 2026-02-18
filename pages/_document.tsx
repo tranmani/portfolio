bb3440992a3641859feaf0b794f1f84c10942854
tranmani <minhhuy8137@gmail.com>
2026-02-18 14:43:09 +0100

    add clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,14 @@ export default class MyDocument extends Document {
       }
     `;
 
+    const clarityTracking = `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "vjbt90ua1x");
+    `;
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -37,8 +45,9 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
-            {/* <script dangerouslySetInnerHTML={{ __html: logging }} /> */}
+            <script dangerouslySetInnerHTML={{ __html: logging }} />
             <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+            <script dangerouslySetInnerHTML={{ __html: clarityTracking }} />
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
